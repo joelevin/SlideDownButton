c9237a380d21f38c605b0ee2197a124544cd18c7
Joe Levin <jlevin@apppartner.com>
2014-11-13 15:45:56 -0500

    Initial Commit

---
+++ Google ButtonTests/Google_ButtonTests.m
@@ -0,0 +1,40 @@
+//
+//  Google_ButtonTests.m
+//  Google ButtonTests
+//
+//  Created by App Partner on 11/13/14.
+//  Copyright (c) 2014 App Partner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Google_ButtonTests : XCTestCase
+
+@end
+
+@implementation Google_ButtonTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
