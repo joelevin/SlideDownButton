c5b2a197d4c078b024178fb616fa3497ed6f4d8e
Joe Levin <jlevin@apppartner.com>
2014-11-13 19:08:36 -0500

    added button subclass

---
+++ Google Button/ExpandoButton.h
@@ -0,0 +1,13 @@
+//
+//  ExpandoButton.h
+//  Google Button
+//
+//  Created by App Partner on 11/13/14.
+//  Copyright (c) 2014 App Partner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExpandoButton : UIControl
+
+@end
---
+++ Google Button/ExpandoButton.m
@@ -0,0 +1,21 @@
+//
+//  ExpandoButton.m
+//  Google Button
+//
+//  Created by App Partner on 11/13/14.
+//  Copyright (c) 2014 App Partner. All rights reserved.
+//
+
+#import "ExpandoButton.h"
+
+@implementation ExpandoButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
